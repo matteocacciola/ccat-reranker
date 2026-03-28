fb4fc2915028312568a43595ba85d0054f3c7c7c
Matteo Cacciola <matteo.cacciola@gmail.com>
2026-03-28 03:17:26 +0100

    Enhance SBERT model loading with caching and logging
    Added logging and model caching for SBERT in ccat_reranker.py.

--- ccat_reranker.py
+++ ccat_reranker.py
@@ -1,7 +1,9 @@
-from cat import hook, RecallSettings
+from cat import hook, log, RecallSettings
 from sentence_transformers import CrossEncoder
 
 from .rankers import litm, sbert_ranker
+
+_MODELS_CACHE = {}
 
 
 @hook(priority=1)
@@ -13,9 +15,17 @@ def after_cat_recalls_memories(config: RecallSettings, cat) -> None:
 
     final_docs = None
     if settings["sbert"]:
-        model = CrossEncoder(settings["ranker"])
+        model_name = settings["ranker"]
+        
+        if model_name not in _MODELS_CACHE:
+            log.debug(f"Loading SBERT model: {model_name}...")
+            _MODELS_CACHE[model_name] = CrossEncoder(model_name)
+        
+        model = _MODELS_CACHE[model_name]
         final_docs = sbert_ranker(
-            cat.working_memory.context_memories, cat.working_memory.history[0].content.text, model
+            cat.working_memory.context_memories,
+            cat.working_memory.history[0].content.text,
+            model,
         )
 
     if settings["litm"]:
